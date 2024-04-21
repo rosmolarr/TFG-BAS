0c879b8fb0cb366922eef80b0922a7104807d371
Rosa Maria Molina Arregui <rosmolarr@alum.us.es>
2024-04-21 18:09:12 +0200

    a ver si ahora si funciona todo

--- src/main/java/org/springframework/samples/bas/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/bas/configuration/SecurityConfiguration.java
@@ -1,20 +1,12 @@
 package org.springframework.samples.bas.configuration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.util.Arrays;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.samples.bas.configuration.jwt.AuthEntryPointJwt;
 import org.springframework.samples.bas.configuration.jwt.AuthTokenFilter;
 import org.springframework.samples.bas.configuration.services.UserDetailsServiceImpl;
@@ -28,10 +20,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -52,6 +40,7 @@ public class SecurityConfiguration {
 
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.requiresChannel(requiresChannel -> requiresChannel.anyRequest().requiresSecure());
 		
 		http
 			.cors(withDefaults())		
