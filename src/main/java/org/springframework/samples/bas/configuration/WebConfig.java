8b1829072c2a85783ff56993127c8c70fd346c15
Rosa Maria Molina Arregui <rosmolarr@alum.us.es>
2024-04-21 17:02:04 +0200

    a ver si ahora funciona

--- src/main/java/org/springframework/samples/bas/configuration/WebConfig.java
+++ src/main/java/org/springframework/samples/bas/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package org.springframework.samples.bas.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
@@ -15,4 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(idToEntityConverter);
     }
     
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "https://tfg-bas.vercel.app",
+                "https://tfg-bas-git-main-rosmolarrs-projects.vercel.app",
+                "https://tfg-kpuul0z51-rosmolarrs-projects.vercel.app"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
+    }
 }
