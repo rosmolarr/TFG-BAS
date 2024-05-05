2585fa9f97164078b3de90be0ce3f5ee93e581b1
Rosa Maria Molina Arregui <rosmolarr@alum.us.es>
2024-05-05 19:59:53 +0200

    secrets

--- src/main/java/org/springframework/samples/bas/BasInitializer.java
+++ src/main/java/org/springframework/samples/bas/BasInitializer.java
@@ -1,7 +1,13 @@
 package org.springframework.samples.bas;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import jakarta.annotation.PostConstruct;
 
 public class BasInitializer extends SpringBootServletInitializer {
 
@@ -10,4 +16,18 @@ public class BasInitializer extends SpringBootServletInitializer {
 		return application.sources(BasApplication.class);
 	}
 
+	@PostConstruct
+    public void init() {
+        Properties props = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(".secrets")) {
+            props.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String username = props.getProperty("DB_USERNAME");
+        String password = props.getProperty("DB_PASSWORD");
+
+        // Usa las variables username y password según sea necesario
+    }
 }
